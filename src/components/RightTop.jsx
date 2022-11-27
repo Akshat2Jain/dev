ae97bcd6840dd5393d2f46a194619d2d49e94bfa
Mayank1170 <agarwalmayank1170@gmail.com>
2022-11-27 17:23:06 +0530

    Break top and bottom into coponents

---
+++ src/components/RightTop.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import gold from "../assets/gold2.png";
+import silver from "../assets/silver.png";
+import bronze from "../assets/bronze.png";
+const RightTop = () => {
+  return (
+    <>
+      <div>
+        <div className="col-span-1 md:col-span-2 flex flex-col justify-between">
+          <h1 className="text-2xl font-bold mb-8">Your Achievements 🤩</h1>
+          <div className="bg-white p-8 rounded-xl shadow-2xl">
+            <div className="flex items-center gap-4 mb-8">
+              <img
+                src={gold}
+                className="w-14 h-14 object-cover rounded-full"
+                alt="img"
+              />
+              <div>
+                <h3 className="font-bold">Gold Badge</h3>
+                <p className="text-gray-500">
+                  For completing this much of questions
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center gap-4 mb-4">
+              <img
+                src={silver}
+                className="w-14 h-14 object-cover rounded-full"
+                alt="img"
+              />
+              <div>
+                <h3 className="font-bold">Silver Badge</h3>
+                <p className="text-gray-500">
+                  For completing this much of questions
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center gap-4 mb-4">
+              <img
+                src={bronze}
+                className="w-14 h-14 object-cover rounded-full"
+                alt="img"
+              />
+              <div>
+                <h3 className="font-bold">Bronze Badge</h3>
+                <p className="text-gray-500">
+                  For completing this much of questions
+                </p>
+              </div>
+            </div>
+
+            <div className="flex justify-end">
+              <a
+                href="#"
+                className="hover:text-primary-100 transition-colors hover:underline"
+              >
+                See all Achievements
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default RightTop;
