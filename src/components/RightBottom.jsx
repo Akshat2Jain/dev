7dd674756fc1b285fbb132e25fe37ad296d67d1a
Akshatjain <akshatj@cabin4j.com>
2022-12-29 13:08:03 +0530

    corrected spelling

--- src/components/RightBottom.jsx
+++ src/components/RightBottom.jsx
@@ -48,7 +48,7 @@ const RightBottom = () => {
         <div class=" w-full p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white font-serif">
-              A complete roadmap to become a Frontend Devloper 
+              A complete roadmap to become a Frontend Developer 
             </h5>
           </a>
           <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
